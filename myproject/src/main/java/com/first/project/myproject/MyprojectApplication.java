b24a0af3a18671c65f13fe7abcf8af556e0ce7b0
manojmlv <manojkumarmalviya19@gmail.com>
2022-02-12 16:40:06 +0530

    first spring boot setup

---
+++ myproject/src/main/java/com/first/project/myproject/MyprojectApplication.java
@@ -0,0 +1,14 @@
+package com.first.project.myproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyprojectApplication {
+
+	public static void main(String[] args) {
+		System.out.print("hello manoj hi");
+		SpringApplication.run(MyprojectApplication.class, args);
+	}
+
+}
